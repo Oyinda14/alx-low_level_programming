b56d66698f7683248cd9d1d1a58b1e216667a1f1
Oyindamola <oyindamolamary526@gmail.com>
2022-09-10 03:38:38 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ * (*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		return (0);
+	}
