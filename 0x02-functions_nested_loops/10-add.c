c1513516243db1b0b6e8c089e9108b96d241c4a1
Oyindamola <oyindamolamary526@gmail.com>
2022-09-13 21:17:07 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,25 @@
 
 /**
  * add - add two integers and returns the result
- *  @a: two integer arguments
- *  @b: two integer arguments
- *  Return: sum of the result
- *  /
+ * @a: two integer arguments
+ * @b: two integer arguments
+ * Return: sum of the result
+ */
 
 int add(int a, int b)
 {
 	return (a + b);
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
